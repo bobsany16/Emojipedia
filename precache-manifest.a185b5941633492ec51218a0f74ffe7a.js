96ae6a402d247b8ff5058cdd0d9f4412918a5326
bobsany16 <46255649+bobsany16@users.noreply.github.com>
2020-10-04 23:43:20 +0000

    Deploying to gh-pages from  @ 24635cfa902984b3f72d7ee50ddeecacb67c198a 🚀

---
+++ precache-manifest.a185b5941633492ec51218a0f74ffe7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1069e007f7e941e8d8e324f8830fd9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2f441e26b5c355e6c2",
+    "url": "/static/js/2.eb89d915.chunk.js"
+  },
+  {
+    "revision": "7a83ba0e8c9529ac465b7e27a737487f",
+    "url": "/static/js/2.eb89d915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "671d6df8e10b937df129",
+    "url": "/static/js/main.7f67cbeb.chunk.js"
+  },
+  {
+    "revision": "13fb8c5fa137029ee659",
+    "url": "/static/js/runtime-main.83ecf5fb.js"
+  }
+]);
